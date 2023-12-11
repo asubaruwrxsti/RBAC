64e51aa7174b0fd4ca6d6ca87bc9f0512c6a2659
Arlind <arlind12344@gmail.com>
2023-12-11 15:10:16 +0100

    Update imports and middleware configurations

--- main.go
+++ main.go
@@ -1,55 +1,23 @@
 package main
 
 import (
-	"github.com/gofiber/contrib/fiberzap"
 	"github.com/gofiber/fiber/v2" // import the fiber package
-	"github.com/gofiber/template/html/v2"
 
 	"log"
 
 	"RBAC/database"
 	"RBAC/router"
-
-	_ "github.com/lib/pq"
-
-	"github.com/gofiber/swagger"
-	"go.uber.org/zap"
 )
 
-// @title Go Fiber REST API with Swagger Example
-// @description Go Fiber REST API with Swagger Example
-// @version 1
-// @host localhost:3000
-// @BasePath /
-// @schemes http
 func main() {
 
 	if err := database.Connect(); err != nil {
 		log.Fatal(err)
 	}
 
-	engine := html.New("./views", ".html")
-	app := fiber.New(
-		fiber.Config{
-			Views: engine,
-		},
-	)
+	app := fiber.New(fiber.Config{})
 
-	logger, _ := zap.NewProduction()
-
-	// app.Use(middleware.Logger())
 	router.SetupRoutes(app)
 
-	app.Get("/swagger/*", swagger.HandlerDefault)     // default
-	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-		URL:         "http://localhost:3000/swagger/doc.json",
-		DeepLinking: false,
-	}))
-
-	app.Use(fiberzap.New(fiberzap.Config{
-		Logger: logger,
-	}))
-
 	log.Fatal(app.Listen(":3000"))
-
 }
