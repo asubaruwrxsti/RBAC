35ec2c60b459b243dc8c25522db0f5c503931029
Arlind <arlind12344@gmail.com>
2023-12-11 17:08:32 +0100

    Refactor

--- handler/handler.go
+++ handler/handler.go
@@ -16,7 +16,7 @@ func ValidateToken(c *fiber.Ctx) error {
 	// Read the value of the "Authorization" header
 	token := c.Get("Authorization")[7:]
 
-	// parse the jwt token
+	// Parse the JWT token
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
@@ -26,11 +26,6 @@ func ValidateToken(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	// Print the token to the console (optional)
-	fmt.Printf("Token from header: %s\n", token)
-	// get the claims
-	fmt.Printf("Claims -> UserID: %+v\n", parsedToken.Claims.(jwt.MapClaims)["userId"])
-	fmt.Printf("Claims -> GroupId: %+v\n", parsedToken.Claims.(jwt.MapClaims)["groupId"])
 	return c.JSON(fiber.Map{
 		"userId":  parsedToken.Claims.(jwt.MapClaims)["userId"],
 		"groupId": parsedToken.Claims.(jwt.MapClaims)["groupId"],
