47d688c94f979fa723a012a63cdca936db3ceedf
Arlind <arlind12344@gmail.com>
2023-12-13 14:01:15 +0100

    Added scenario for unauthorized user

--- handler/handler.go
+++ handler/handler.go
@@ -20,15 +20,32 @@ func ValidateToken(c *fiber.Ctx) error {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
+	// Check if the token is valid
+	if !parsedToken.Valid {
+		fmt.Println("Token is invalid")
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	userId := parsedToken.Claims.(jwt.MapClaims)["userId"]
+	groupId := int(parsedToken.Claims.(jwt.MapClaims)["groupId"].(float64))
+	fmt.Println("userId: ", userId)
+	fmt.Println("groupId: ", groupId)
+
+	// Simulate where the user does not have permission to access this resource
+	if groupId == 2 {
+		fmt.Println("User does not have permission to access this resource, groupId: ", groupId)
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
 
 	if err != nil {
 		fmt.Printf("Error parsing token: %v\n", err)
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
+	fmt.Println("<< Token is valid, returning ...")
 
 	return c.JSON(fiber.Map{
-		"userId":  parsedToken.Claims.(jwt.MapClaims)["userId"],
-		"groupId": parsedToken.Claims.(jwt.MapClaims)["groupId"],
+		"userId":  userId,
+		"groupId": groupId,
 		"token":   token,
 		"message": "Token is valid",
 	})
