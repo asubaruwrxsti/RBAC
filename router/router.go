33f843da26312d6d5bfe59563cccdf67a45aed2a
Arlind <arlind12344@gmail.com>
2023-12-14 11:55:49 +0100

    Updated the router with the new changes

--- router/router.go
+++ router/router.go
@@ -5,10 +5,18 @@ import (
 	"RBAC/middleware"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
-func SetupRoutes(app *fiber.App) {
+func SetupRoutes(app *fiber.App, dbConn *gorm.DB) {
+	authMiddleware := middleware.NewAuth().
+		Use(middleware.RequestToken()).
+		Use(middleware.VerifyToken())
+
 	app.Get("/", handler.Home)
-	app.Get("/auth", middleware.RequestToken())
-	app.Get("/authreq", middleware.VerifyToken(), handler.ValidateToken)
+	app.Get("parse", handler.ParseToken)
+
+	app.Get("/authreq", authMiddleware.Apply, func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 }
